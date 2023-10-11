e710b7aaf21f44963ef9dd9a97d1efe85cd3429e
Yuri Cruz <yuricruzf@gmail.com>
2023-10-11 07:59:31 -0300

    Adding more contacts itens in footer

--- src/app/components/Footer/page.tsx
+++ src/app/components/Footer/page.tsx
@@ -11,11 +11,17 @@ export function Footer() {
 						escritório" title="Logo do escritório" />
                 <ul className={styles.rodape_lista}>
                     <li><a href="https://github.com/YuriCF1" target="_blank" rel="noopener
-								noreferrer"><img className={styles.logo_git}
+								noreferrer"><img className={styles.logo_social}
                             src="assets/img/github.png" alt="Perfil no GitHub" /></a></li>
                     <li><a href="https://www.linkedin.com/in/yuricf/" target="_blank"
                         rel="noopener noreferrer"><img
-                            className={styles.logo_git} src="assets/img/linkedin.png" alt="Perfil no GitHub" /></a></li>
+                            className={styles.logo_social} src="assets/img/linkedin.png" alt="Perfil no GitHub" /></a></li>
+                    <li><a href="mailto:yurifdev@gmail.com" target="_blank">
+                        <img src="/img/icons/email.svg" alt="Ícone GitHub" className={styles.logo_social} />
+                    </a></li>
+                    <li><a href="https://wa.me/5582988439390?text=Olá,%20Yuri!" target="_blank">
+                        <img src="/img/icons/wpp.svg" alt="Ícone Whatsapp" className={styles.logo_social} />
+                    </a></li>
                 </ul>
             </div>
             <p className={styles.copyright}>&copy; Todos os direitos reservados - {anoString}</p>
