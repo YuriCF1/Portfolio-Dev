93f37a4b2177b75f9d1cd89d01932940126e3821
Yuri Cruz <yuricruzf@gmail.com>
2023-10-11 07:57:45 -0300

    Creating button

---
+++ src/app/components/ButtonTop/ButtonTop.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image"
+
+import styles from "./Arrow.module.css"
+
+export function ButtonTop() {
+    return (
+        <>
+            <a href="#home">
+                <Image src="/img/icons/arrow.svg" title="Logo da escritório" alt="Ícone de seta para cima" width={50} height={50} priority={true} className={styles.arrow}/>
+            </a>
+        </>
+    )
+}
+
+export default ButtonTop
