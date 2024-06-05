c8fafdcab87535eb61b555e17a4f89850e6a0b55
Yuri Cruz <yuricruzf@gmail.com>
2024-06-05 01:37:21 -0300

    Changing animation

--- src/app/components/About/page.tsx
+++ src/app/components/About/page.tsx
@@ -1,17 +1,32 @@
-import styles from "./About.module.css"
+'use client'
 
+import { useContext, useEffect, useState } from "react"
+import styles from "./About.module.css"
+import { LanguageContext } from "@/app/context/toggleConext"
 
 export function About() {
+
+    const [paraCursor, setParaCursor] = useState(false)
+
+    const { language, setLanguage } = useContext(LanguageContext)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setParaCursor(true)
+        }, 7000);
+    }, [])
+
     return (<>
         <section>
             <div className={styles.sobre_container} id="sobre">
                 <h2 className={styles.sobre_titulo}>Quem sou eu</h2>
                 <div className={styles.versaoPC}>
-                    <p className={`${styles.sobre_texto} ${styles.typing_animation}`} id="servicos">Olá! Me chamo Yuri.
-                        Sou Desenvolvedor frontend
-                        com conhecimento em backend,
-                        e pretendo ser um exímio fullstack developer. E amo tudo isso {`=)`}
+                    <p className={paraCursor ? `${styles.sobre_texto} ${styles.borda_transparente}` : `${styles.sobre_texto} ${styles.typing_animationDesktop1}`} id="servicos">Olá! Me chamo Yuri.
+                        Sou Desenvolvedor Frontend
+                        com conhecimento também em Backend,
+                        e pretendo ser um exímio Fullstack/Mobile Developer.
                     </p>
+                    <p className={`${styles.sobre_texto} ${styles.typing_animationDesktop2}`} >E amo tudo isso {`=)`}</p>
                 </div>
                 <div className={styles.versaoMob}>
                     <p className={`${styles.sobre_texto2} ${styles.typing_animation1}`} id="servicos">Olá! Me chamo Yuri.
