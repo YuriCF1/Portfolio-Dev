3d3827ef3e398d1cfd2a13ed0d6797ddfa6e53bd
Yuri Cruz <yuricruzf@gmail.com>
2024-06-06 18:34:21 -0300

    Google analytics tag 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,19 @@ export default function RootLayout({
           }}
         />
         {/* End Google Tag Manager */}
+        {/* Google Analytics Manager */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TY93XBCPZY"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-TY93XBCPZY');
+              `,
+          }}
+        />
+        {/* End Google Analytics Manager */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Share+Tech&display=swap" rel="stylesheet" />
