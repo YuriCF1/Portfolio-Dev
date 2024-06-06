ee0dbd67b5cee0a407ca2a5adb6d39b34b8d5efb
Yuri Cruz <yuricruzf@gmail.com>
2024-06-06 18:48:33 -0300

    Cleaning tags that are not being used

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,17 +19,6 @@ export default function RootLayout({
     <html lang="pt-br">
       <AOSInit />
       <head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-NR2R7HLH');`,
-          }}
-        />
-        {/* End Google Tag Manager */}
         {/* Google Analytics Manager */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-TY93XBCPZY"></script>
         <script
@@ -50,14 +39,6 @@ export default function RootLayout({
         <title>Yuri Cruz - Portfolio</title>
       </head>
       <body className={inter.className}>{children}</body>
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-NR2R7HLH"
-          height="0"
-          width="0"
-          style={{ display: 'none', visibility: 'hidden' }}
-        ></iframe>
-      </noscript>
     </html>
   )
 }
