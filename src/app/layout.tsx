997300c5a94aab60bf59ec18b8bb3ef879667c1b
Yuri Cruz <yuricruzf@gmail.com>
2024-06-06 18:19:41 -0300

    Adding google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,17 @@ export default function RootLayout({
     <html lang="pt-br">
       <AOSInit />
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-NR2R7HLH');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Share+Tech&display=swap" rel="stylesheet" />
@@ -26,6 +37,14 @@ export default function RootLayout({
         <title>Yuri Cruz - Portfolio</title>
       </head>
       <body className={inter.className}>{children}</body>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-NR2R7HLH"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
     </html>
   )
 }
