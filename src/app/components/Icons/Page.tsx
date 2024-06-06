c78f98d6d997b3525068b6d394ef6aa70542721b
Yuri Cruz <yuricruzf@gmail.com>
2024-06-06 01:12:27 -0300

    Creating icon area

---
+++ src/app/components/Icons/Page.tsx
@@ -0,0 +1,93 @@
+import s from "./Icons.module.css"
+
+const Icons = () => {
+    return (
+        <section className={s.secao} data-aos="zoom-in" data-aos-delay="1000" data-aos-duration="2000">
+            <h3>Linguages e frameworks que tenho experiência</h3>
+            <div className={s.iconsL}>
+                <div>
+                    <img alt="Symbol-HTML" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" width="35" height="35" />
+                    <span>HTML</span>
+                </div>
+                <div>
+                    <img alt="Symbol-CSS" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" width="35" height="35" />
+                    <span>CSS</span>
+                </div>
+                <div>
+                    <img alt="sass symbol" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" width="40" height="40" />
+                    <span>Sass</span>
+                </div>
+                <div>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" width="35" height="35" />
+                    <span>Bootstrap</span>
+                </div>
+                <div>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" width="45" height="45" />
+                    <span>Tailwind CSS</span>
+                </div>
+                <div>
+                    <img alt="Symbol-JavaScript" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" width="35" height="35" />
+                    <span>JavaScript</span>
+                </div>
+                <div>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" width="35" height="35" />
+                    <span>TypeScript</span>
+                </div>
+                <div>
+                    <img alt="Symbol-jQuery" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-plain-wordmark.svg" width="40" height="40" />
+                    <span>jQuery</span>
+                </div>
+                <div>
+                    <img alt="react symbol" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" width="40" height="40" />
+                    <span>React</span>
+                </div>
+                <div>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" width="40" height="40" />
+                    <span>Next.js</span>
+                </div>
+                <div>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" width="40" height="40" />
+                    <span>Jest</span>
+                </div>
+                <div>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg" width="45" height="45" />
+                    <span>AngularJS</span>
+                </div>
+                <div>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rxjs/rxjs-original.svg" width="40" height="40" />
+                    <span>RxJS</span>
+                </div>
+                <div>
+                    <img alt="wordpress symbol" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-plain.svg" width="40" height="40" />
+                    <span>WordPress</span>
+                </div>
+                <div>
+                    <img alt="firebase symbol" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain-wordmark.svg" width="40" height="40" />
+                    <span>Firebase</span>
+                </div>
+                <div>
+                    <img alt="mongo DB" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain-wordmark.svg" width="40" height="40" />
+                    <span>MongoDB</span>
+                </div>
+                <div>
+                    <img alt="mongo DB" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg" width="40" height="40" />
+                    <span>Postman</span>
+                </div>
+                <div>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" width="40" height="40" />
+                    <span>Node.js</span>
+                </div>
+                <div>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" width="40" height="40" />
+                    <span>PostgreSQL</span>
+                </div>
+                <div>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" width="40" height="40" />
+                    <span>Git</span>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Icons
