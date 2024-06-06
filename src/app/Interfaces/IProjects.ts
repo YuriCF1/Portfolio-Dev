b0d122b903cb0ee27501365a97bf85927e5b6dd2
Yuri Cruz <yuricruzf@gmail.com>
2024-06-06 01:11:50 -0300

    Addinf interfacE

---
+++ src/app/Interfaces/IProjects.ts
@@ -0,0 +1,10 @@
+export default interface Project {
+    title: string;
+    description: string;
+    img: string;
+    altImg: string;
+    techs: string[];
+    techDescriptions?: string[];
+    linkGH: string;
+    linkDeploy: string
+}
