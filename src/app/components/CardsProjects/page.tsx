8544f56edf2308b2caa13abe437290de5ab34fc2
Yuri Cruz <yuricruzf@gmail.com>
2024-06-06 01:12:57 -0300

    Refactoring

---
+++ src/app/components/CardsProjects/page.tsx
@@ -0,0 +1,58 @@
+import { useContext, useEffect, useState } from "react";
+import projectsPortuguese from "../../api/projects.json"
+import projectEnglish from "../../api/ProjectsEnglish.json"
+import styles from "./Cards.module.css"
+import Image from "next/image"
+import { LanguageContext } from "@/app/context/toggleConext";
+import IProject from "../../Interfaces/IProjects"
+
+const Cards = () => {
+    const { language } = useContext(LanguageContext)
+
+    const [srcProjects, setSrcProjects] = useState(projectsPortuguese);
+
+    useEffect(() => {
+        const srcProject = language === "portuguese" ? projectsPortuguese : projectEnglish;
+        setSrcProjects(srcProject);
+    }, [language]);
+
+    return (
+        <div className={styles.cardSection}>
+            {srcProjects.projects.map((project: IProject, index: number) => (
+                <ul className={styles.title} key={project.title}>
+                    <li>
+                        <strong className={styles.cardTitle}>{language && language === "portuguese" ? "Título" : "Title"}:</strong> <p>{project.title}</p>
+                    </li>
+                    <li>
+                        <strong>{language && language === "portuguese" ? "Descrição" : "Description"}:</strong> <p>{project.description} </p>
+                    </li>
+                    {/* Tem que fazer isso para que o tamanho da imagem abaixo consiga ser modificada pelo css */}
+                    <div className={styles.divImage} >
+                        <a href={project.linkDeploy} >
+                            <Image src={project.img} alt={project.altImg} objectFit="cover" layout="fill" className={styles.exampleImg} />
+                        </a>
+                    </div>
+                    <li className={styles.techsContainer}>
+                        <strong>{language && language === "portuguese" ? "Tecnologias" : "Techs"}</strong>
+                        <div>
+                            {project.techs.map((tech) => (
+                                <img key={project.techDescriptions![index]} src={tech} alt={project.techDescriptions && project.techDescriptions[index] ? project.techDescriptions[index] : 'Descrição da tecnologia'} className={styles.techs} />
+                            ))}
+                        </div>
+                    </li>
+                    <li className={styles.techsContainer}>
+                        <strong>Deploys:</strong>
+                        <a href={project.linkGH} target="_blank">
+                            <Image src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg' alt="Ícone do GitHub" width={25} height={25} className={styles.techs} />
+                        </a>
+                        <a href={project.linkDeploy} target="_blank">
+                            <Image src='/img/svg-internet.svg' alt="Ícone do GitHub" width={25} height={25} className={styles.techs} />
+                        </a>
+                    </li>
+                </ul>
+            ))}
+        </div>
+    )
+}
+
+export default Cards
--- src/app/components/Projects/pages.tsx
+++ src/app/components/Projects/pages.tsx
@@ -1,47 +1,29 @@
 'use client'
 
 import { useContext, useEffect, useState } from "react";
-import projectsPortuguese from "../../api/projects.json"
-import projectEnglish from "../../api/ProjectsEnglish.json"
 import styles from "./Projects.module.css"
 import Image from "next/image"
 import { LanguageContext } from "@/app/context/toggleConext";
 import profilePic from "../../../../public/img/Profile.jpg"
-
-interface Project {
-  title: string;
-  description: string;
-  img: string;
-  altImg: string;
-  techs: string[];
-  techDescriptions?: string[];
-  linkGH: string;
-  linkDeploy: string
-}
-
+import Icons from "../Icons/Page";
+import Cards from "../CardsProjects/page";
 export default function Projects() {
 
   const { language } = useContext(LanguageContext)
-
-  const [srcProjects, setSrcProjects] = useState(projectsPortuguese);
-
-  useEffect(() => {
-    const srcProject = language === "portuguese" ? projectsPortuguese : projectEnglish;
-    setSrcProjects(srcProject);
-  }, [language]);
-
-
 
   return (
     <div data-aos="fade-down"
       data-aos-easing="linear"
       data-aos-duration="1500">
       <h1 className={styles.mainTitle}>{language && language === "portuguese" ? "Projetos desenvolvidos" : "Developed Projects"}</h1>
+      <Icons />
       <section className={styles.board} id="projetos" >
         <div className={styles.profileImgContainer}>
           <div className={styles.divProfImage}>
-            {/* <Image src='https://github.com/YuriCF1.png' objectFit="cover" layout="fill" alt="Foto de Perfil Yuri Cruz" className={styles.profileImg} /> */}
-            <Image src={profilePic} objectFit="cover" layout="fill" alt="Foto de Perfil Yuri Cruz" className={styles.profileImg} />
+            <a href="https://wa.me/5582988439390?text=Olá,%20Yuri!" target="_blank">
+              {/* <Image src='https://github.com/YuriCF1.png' objectFit="cover" layout="fill" alt="Foto de Perfil Yuri Cruz" className={styles.profileImg} /> */}
+              <Image src={profilePic} objectFit="cover" layout="fill" alt="Foto de Perfil Yuri Cruz" className={styles.profileImg} />
+            </a>
           </div>
           <ul>
             <li><a href="https://www.linkedin.com/in/yuricf/" target="_blank">
@@ -69,41 +51,7 @@ export default function Projects() {
             "Des. Frontend HTML/CSS/JS/TS/Next/React/Angular/Vue/MySQL/MongoDB/Firebase. Usuário do Figma, Blender, Photoshop e Premiere."
             : "Frontend dev. HTML/CSS/JS/TS/Next/React/Angular/Vue/MySQL/MongoDB/Firebase. User of design Figma, Blender, Photoshop and Premiere."}</p>
         </div>
-        <div className={styles.cardSection}>
-          {srcProjects.projects.map((project: Project, index: number) => (
-            <ul className={styles.title} key={project.title}>
-              <li>
-                <strong className={styles.cardTitle}>{language && language === "portuguese" ? "Título" : "Title"}:</strong> <p>{project.title}</p>
-              </li>
-              <li>
-                <strong>{language && language === "portuguese" ? "Descrição" : "Description"}:</strong> <p>{project.description} </p>
-              </li>
-              {/* Tem que fazer isso para que o tamanho da imagem abaixo consiga ser modificada pelo css */}
-              <div className={styles.divImage} >
-                <a href={project.linkDeploy} >
-                  <Image src={project.img} alt={project.altImg} objectFit="cover" layout="fill" className={styles.exampleImg} />
-                </a>
-              </div>
-              <li className={styles.techsContainer}>
-                <strong>{language && language === "portuguese" ? "Tecnologias" : "Techs"}</strong>
-                <div>
-                  {project.techs.map((tech) => (
-                    <img key={project.techDescriptions![index]} src={tech} alt={project.techDescriptions && project.techDescriptions[index] ? project.techDescriptions[index] : 'Descrição da tecnologia'} className={styles.techs} />
-                  ))}
-                </div>
-              </li>
-              <li className={styles.techsContainer}>
-                <strong>Deploys:</strong>
-                <a href={project.linkGH} target="_blank">
-                  <Image src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg' alt="Ícone do GitHub" width={25} height={25} className={styles.techs} />
-                </a>
-                <a href={project.linkDeploy} target="_blank">
-                  <Image src='/img/svg-internet.svg' alt="Ícone do GitHub" width={25} height={25} className={styles.techs} />
-                </a>
-              </li>
-            </ul>
-          ))}
-        </div>
+        <Cards />
       </section>
     </div>
   )
