c9db2d5ff283bcd2e54d17421d374c897a3a36e1
Yuri Cruz <yuricruzf@gmail.com>
2024-06-19 03:30:44 -0300

    Adding new index

--- src/app/components/CardsProjects/page.tsx
+++ src/app/components/CardsProjects/page.tsx
@@ -21,28 +21,28 @@ const Cards = () => {
     return (
         <div className={styles.cardSection}>
             {srcProjects.projects.map((project: IProject, index: number) => (
-                <ul className={styles.title} key={project.title}>
-                    <li>
+                <ul className={styles.title} key={index}>
+                    <li key={`title-${index}`}>
                         <strong className={styles.cardTitle}>{language && language === "portuguese" ? "Título" : "Title"}:</strong> <p>{project.title}</p>
                     </li>
-                    <li>
-                        <strong>{language && language === "portuguese" ? "Descrição" : "Description"}:</strong> <p>{project.description} </p>
+                    <li key={`description-${index}`}>
+                        <strong>{language && language === "portuguese" ? "Descrição" : "Description"}:</strong> <p> {project.description} </p>
                     </li>
                     {/* Tem que fazer isso para que o tamanho da imagem abaixo consiga ser modificada pelo css */}
-                    <div className={styles.divImage} >
-                        <a href={project.linkDeploy} >
+                    <div className={styles.divImage}>
+                        <a href={project.linkDeploy}>
                             <Image src={project.img} alt={project.altImg} objectFit="cover" layout="fill" className={styles.exampleImg} />
                         </a>
                     </div>
-                    <li className={styles.techsContainer}>
+                    <li key={`techs-${index}`} className={styles.techsContainer}>
                         <strong>{language && language === "portuguese" ? "Tecnologias" : "Techs"}</strong>
                         <div>
-                            {project.techs.map((tech) => (
-                                <img key={project.techDescriptions![index]} src={tech} alt={project.techDescriptions && project.techDescriptions[index] ? project.techDescriptions[index] : 'Descrição da tecnologia'} className={styles.techs} />
+                            {project.techs.map((tech, techIndex) => (
+                                <img key={`tech-${index}-${techIndex}`} src={tech} alt={project.techDescriptions && project.techDescriptions[techIndex] ? project.techDescriptions[techIndex] : 'Descrição da tecnologia'} className={styles.techs} />
                             ))}
                         </div>
                     </li>
-                    <li className={styles.techsContainer}>
+                    <li key={`deploy-${index}`} className={styles.techsContainer}>
                         <strong>Deploys:</strong>
                         <a href={project.linkGH} target="_blank">
                             <Image src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg' alt="Ícone do GitHub" width={25} height={25} className={styles.techs} />
